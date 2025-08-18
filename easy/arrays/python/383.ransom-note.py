5ab77d6eb0036997f6cd9438802165586e690efe
Tony C <goldcar400@yahoo.com>
2025-08-18 11:43:10 -0700

    downloaded 383. Random Notes code

---
+++ easy/arrays/python/383.ransom-note.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=383 lang=python3
+#
+# [383] Ransom Note
+#
+# https://leetcode.com/problems/ransom-note/description/
+#
+# algorithms
+# Easy (64.70%)
+# Likes:    5453
+# Dislikes: 532
+# Total Accepted:    1.7M
+# Total Submissions: 2.6M
+# Testcase Example:  '"a"\n"b"'
+#
+# Given two strings ransomNote and magazine, return true if ransomNote can be
+# constructed by using the letters from magazine and false otherwise.
+# 
+# Each letter in magazine can only be used once in ransomNote.
+# 
+# 
+# Example 1:
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+# Example 2:
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+# Example 3:
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= ransomNote.length, magazine.length <= 10^5
+# ransomNote and magazine consist of lowercase English letters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+# @lc code=end
+
