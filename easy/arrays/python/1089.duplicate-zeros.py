bfe01fc770a90bc71799974cee6803636e9c7a0a
Tony C <goldcar400@yahoo.com>
2025-08-28 21:29:41 -0700

    start index adjustment when zero_count > 0 then zero_count - 1

--- easy/arrays/python/1089.duplicate-zeros.py
+++ easy/arrays/python/1089.duplicate-zeros.py
@@ -64,8 +64,21 @@ class Solution:
             if arr[i] == 0:
                 zero_count += 1
         # Shift elements to the right by the number of zeros found
-
-
+        # start from the last index and move backwards
+        i = n - 1       # read head
+        wr_head = n - 1  # write head
+        if zero_count > 0:
+            zero_count -= 1  # start index adjustment
+        for rd_head in range(n - 1 - zero_count, -1, -1):
+            if arr[rd_head] == 0:
+                arr[wr_head] = 0
+                wr_head -= 1
+                if wr_head < n:
+                    arr[wr_head] = 0    # duplicate zero
+                    wr_head -= 1
+            else:
+                arr[wr_head] = arr[rd_head]
+                wr_head -= 1
 
 
 def main():
