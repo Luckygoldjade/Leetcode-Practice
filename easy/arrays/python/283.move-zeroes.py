494db4891dbc4d713512d62339712f2364ec08cd
Tony C <goldcar400@yahoo.com>
2025-10-05 17:45:51 -0700

    implemented a two pointer one read and one write approach and in place output.

--- easy/arrays/python/283.move-zeroes.py
+++ easy/arrays/python/283.move-zeroes.py
@@ -44,21 +44,45 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        # Two pointers approach. one pointer to for reading the array
+        # and another pointer to keep track of the position to place the next non-zero element.
+        read_ptr = 0
+        write_ptr = 0
+        n = len(nums)
+
+        # read the array. every element needs to be read. so for loop.
+        for read_ptr in range(len(nums)):
+            # yes shift element to left.
+            # one case. element is non-zero.
+            if nums[read_ptr] != 0:
+                nums[write_ptr] = nums[read_ptr]
+                # increment write pointer only when we write a non-zero element.
+                write_ptr += 1
+
+
+        # all the non-zero elements are now at the beginning of the array.
+        # write_ptr is now at the position where the next non-zero element would go.
+        # fill the remaining elements with zero.
+        for i in range(write_ptr, n):
+            nums[i] = 0
+
+        return nums
+    
+
 # @lc code=end
 
 def main():
     sol = Solution()
-
+    print("\n283. Move Zeroes\n")
     # Example 1
     nums = [0,1,0,3,12]
     sol.moveZeroes(nums)
-    print(nums)     # Output should be [1,3,12,0,0]
+    print(nums, "\n")     # Output should be [1,3,12,0,0]
 
     # Example 2
     nums = [0]
     sol.moveZeroes(nums)
-    print(nums)     # Output should be [0]
+    print(nums, "\n")     # Output should be [0]
 
 if __name__ == "__main__":
     main()
